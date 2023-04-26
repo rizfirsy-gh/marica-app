296c86dbc0ae12cc49dc7fa892c1859a86033790
Rizky Firman Syah <rizfirsy@gmail.com>
2023-04-26 22:37:13 +0700

    update: slicing signup page

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,12 +1,61 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, View, Text } from "react-native";
 
 const Signup = () => {
   return (
-    <View>
-      <Text>Signup</Text>
-    </View>
+    <LinearGradient colors={["#FFFFFF", "#DAFAFF"]} style={styles.bgGradient}>
+      <Image
+        source={require("../assets/images/background.png")}
+        resizeMode="cover"
+        resizeMethod="resize"
+        style={{
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+        }}
+      />
+      <View style={styles.top}>
+        <Image
+          source={require("../assets/images/logo.png")}
+          style={{
+            width: 181,
+            height: 76,
+          }}
+        />
+        <Text style={styles.text}>
+          Media belajar sambil bermain yang relevan untuk anak-anak.
+        </Text>
+      </View>
+      {/* TODO: create register form */}
+    </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  bgGradient: {
+    position: "relative",
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "100%",
+    gap: 64,
+  },
+  top: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 8,
+  },
+  text: {
+    color: "#64748B",
+    maxWidth: 250,
+    textAlign: "center",
+    marginTop: 16,
+    marginBottom: 16,
+    fontFamily: "Nunito-Medium",
+  },
+});
 
 export default Signup;
