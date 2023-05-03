98cff130170191c9400867a395219b5f17e17202
Rizky Firman Syah <rizfirsy@gmail.com>
2023-05-03 14:26:30 +0700

    feat: create colors object

--- constant/styles.js
+++ constant/styles.js
@@ -1,8 +1,26 @@
 export const Colors = {
-  //TODO: 3 Mei - create design style guides
-  primary100: "#f9beda",
-  primary500: "#c30b64",
-  primary800: "#610440",
-  error100: "#fcdcbf",
-  error500: "#f37c13",
+  //cyan
+  cyan50: "#ECFEFF",
+  cyan100: "#CFFAFE",
+  cyan200: "#A5F3FC",
+  cyan300: "#67E8F9",
+  cyan400: "#22D3EE",
+  cyan500: "#06B6D4",
+  cyan600: "#0891B2",
+  cyan700: "#0E7490",
+  cyan800: "#155E75",
+  cyan900: "#164E63",
+  cyan950: "#083344",
+  //slate
+  slate50: "#F8FAFC",
+  slate100: "#F1F5F9",
+  slate200: "#E2E8F0",
+  slate300: "#CBD5E1",
+  slate400: "#94A3B8",
+  slate500: "#64748B",
+  slate600: "#475569",
+  slate700: "#334155",
+  slate800: "#1E293B",
+  slate900: "#0F172A",
+  slate950: "#020617",
 };
