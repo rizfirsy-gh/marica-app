79d52bfef7b747b1ed2099a13404fbe34a50c1e9
Rizky Firman Syah <rizfirsy@gmail.com>
2023-05-16 15:45:26 +0700

    update: listing video data

---
+++ assets/videos/videos.js
@@ -0,0 +1,22 @@
+export const videos = [
+  {
+    id: "mG8gR13blEE",
+    title: "Materi Geometri",
+    thumbnail: "geometri.png",
+  },
+  {
+    id: "IoVNH5-xpFY",
+    title: "Materi Aljabar",
+    thumbnail: "aljabar.png",
+  },
+  {
+    id: "qRazIMvmnOM",
+    title: "Materi Bilangan",
+    thumbnail: "bilangan.png",
+  },
+  {
+    id: "fq1a07Jw0Oo",
+    title: "Materi Data",
+    thumbnail: "data.jpg",
+  },
+];
