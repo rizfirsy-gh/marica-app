94dd221b93b1c7ff28ae74721be421894fd58f79
Rizky Firman Syah <rizfirsy@gmail.com>
2023-05-26 11:28:28 +0700

    feat: Create dummy video data

---
+++ assets/media/cerita.js
@@ -0,0 +1,69 @@
+export const dummyCerita = [
+  {
+    title: "Literasi Marica",
+    episodes: [
+      {
+        youtubeId: "mG8gR13blEE",
+        title: "Materi Geometri",
+        thumbnail: "geometri.png",
+      },
+      {
+        youtubeId: "IoVNH5-xpFY",
+        title: "Materi Aljabar",
+        thumbnail: "aljabar.png",
+      },
+      {
+        youtubeId: "qRazIMvmnOM",
+        title: "Materi Bilangan",
+        thumbnail: "bilangan.png",
+      },
+      {
+        youtubeId: "fq1a07Jw0Oo",
+        title: "Materi Data",
+        thumbnail: "data.jpg",
+      },
+    ],
+  },
+  {
+    title: "Main Toca Boca",
+    episodes: [
+      {
+        youtubeId: "i8IhDEvnKGI",
+        title:
+          "Buat Rumah Hello Kitty Untuk Rena! - Toca Boca Eps 01 GoDuplo TV",
+      },
+      {
+        youtubeId: "Y2z-5NJvMCM",
+        title: "Kalian Pernah Main Game Ini Nggak?",
+      },
+      {
+        youtubeId: "mFtSkwC4kp4 ",
+        title: "Drama Pengemis & Si Anak Sombong - Toca Boca Eps 02 GoDuplo TV",
+      },
+      {
+        youtubeId: "2_6UZptOvIM",
+        title: "Drama Hilang di Taman Hiburan - Toca Boca Eps 03 GoDuplo TV",
+      },
+      {
+        youtubeId: "YtiIqTU9iz4",
+        title: "Drama Nene dan Yuka Jadi Mermaid - Toca Boca Eps 04 GoDuplo TV",
+      },
+      {
+        youtubeId: "U4w9xIs2X_g",
+        title: "Drama Wabah Virus Alien - Toca Boca Eps 05 GoDuplo TV",
+      },
+      {
+        youtubeId: "KAsyuttfPhY",
+        title: "Tim pink atau ungu?",
+      },
+      {
+        youtubeId: "KAsyuttfPhY ",
+        title: "Drama Anak Miskin dan Anak Kaya - Toca Boca Eps 06 GoDuplo TV",
+      },
+      {
+        youtubeId: "p29KD122xZM ",
+        title: "Drama Tersesat Ke Rumah Dracula - Toca Boca Eps 07 GoDuplo TV",
+      },
+    ],
+  },
+];
--- screens/Cerita.js
+++ screens/Cerita.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { videos } from "../assets/videos/videos";
+import { dummyCerita } from "../assets/media/cerita";
 import { Button, Text, View, Image, Pressable } from "react-native";
 import { Colors } from "../constant/styles";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
@@ -30,115 +30,56 @@ export default function Cerita({ navigation }) {
           }}
         />
       </View>
-      <ScrollView contentContainerStyle={{ alignItems: "center" }}>
-        <View
-          style={{
-            paddingTop: 24,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 20,
-              fontFamily: "Nunito-SemiBold",
-              color: Colors.slate700,
-            }}
-          >
-            Cerita baru
-          </Text>
+      <ScrollView>
+        {dummyCerita.map((cerita) => (
           <View
+            key={cerita.title}
             style={{
-              flexDirection: "row",
-              gap: 16,
-              marginTop: 8,
+              paddingTop: 24,
             }}
           >
-            <Pressable
-              onPress={() =>
-                navigation.navigate("VideoDetails", {
-                  videoId: "N_oiwAuCpVQ",
-                })
-              }
+            <Text
+              style={{
+                fontSize: 20,
+                fontFamily: "Nunito-SemiBold",
+                color: Colors.slate700,
+              }}
             >
-              <Image
-                source={require("../assets/images/bubu.png")}
-                resizeMode="cover"
-                resizeMethod="resize"
-                style={{
-                  width: 334 / 2,
-                  height: 334 / 2,
-                  borderRadius: 16,
-                }}
-              />
-            </Pressable>
-
-            <Pressable
-              onPress={() =>
-                navigation.navigate("VideoDetails", {
-                  videoId: "N_oiwAuCpVQ",
-                })
-              }
-            >
-              <Image
-                source={require("../assets/images/bubu.png")}
-                resizeMode="cover"
-                resizeMethod="resize"
-                style={{
-                  width: 334 / 2,
-                  height: 334 / 2,
-                  borderRadius: 16,
-                }}
-              />
-            </Pressable>
-          </View>
-        </View>
-        <View
-          style={{
-            paddingTop: 24,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 20,
-              fontFamily: "Nunito-SemiBold",
-              color: Colors.slate700,
-            }}
-          >
-            Naik daun
-          </Text>
-          <View
-            style={{
-              flexDirection: "row",
-              flexWrap: "wrap",
-              gap: 16,
-              marginTop: 8,
-            }}
-          >
-            {videos.map((video) => {
-              return (
-                <Pressable
-                  key={video.id}
-                  onPress={() =>
-                    navigation.navigate("VideoDetails", {
-                      videoId: video.id,
-                      videoTitle: video.title,
-                    })
-                  }
+              {cerita.title}
+            </Text>
+            <View style={{ flexDirection: "row", flexWrap: "wrap", gap: 16 }}>
+              {cerita.episodes.map((episode) => (
+                <View
+                  key={episode.title}
+                  style={{
+                    flexDirection: "row",
+                    gap: 16,
+                    marginTop: 8,
+                  }}
                 >
-                  <Image
-                    source={require(`../assets/images/thumbnail-data.jpg`)}
-                    resizeMode="cover"
-                    resizeMethod="resize"
-                    style={{
-                      width: 334 / 2,
-                      height: 334 / 2,
-                      borderRadius: 16,
-                    }}
-                  />
-                </Pressable>
-              );
-            })}
+                  <Pressable
+                    onPress={() =>
+                      navigation.navigate("VideoDetails", {
+                        videoId: episode.youtubeId,
+                      })
+                    }
+                  >
+                    <Image
+                      source={require("../assets/images/bubu.png")}
+                      resizeMode="cover"
+                      resizeMethod="resize"
+                      style={{
+                        width: 334 / 2,
+                        height: 334 / 2,
+                        borderRadius: 16,
+                      }}
+                    />
+                  </Pressable>
+                </View>
+              ))}
+            </View>
           </View>
-        </View>
+        ))}
       </ScrollView>
     </View>
   );
