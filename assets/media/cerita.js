a62fbe723dc64a2d59945098a01c1773ccf313b0
Rizky Firman Syah <rizfirsy@gmail.com>
2023-05-29 10:50:04 +0700

    add more media (cerita & musik)

--- assets/media/cerita.js
+++ assets/media/cerita.js
@@ -25,6 +25,36 @@ export const dummyCerita = [
     ],
   },
   {
+    title: "Short Literasi Marica",
+    episodes: [
+      {
+        youtubeId: "xM_0EnIKI8I",
+        title: "Ragam Wayang Indonesia - Literasi Budaya",
+      },
+      {
+        youtubeId: "aDgf2mnsHbg",
+        title: "Seberapa Cepat Internet Indonesia - Literasi Digital",
+      },
+      {
+        youtubeId: "CojbLXT3o-E",
+        title: "Apa Mungkin Internet Mati? - Literasi Digital",
+      },
+      {
+        youtubeId: "RO8s_kHoh88",
+        title: "Bakau Si Penjaga Pantai - Literasi Sains",
+      },
+      {
+        youtubeId: "ZcCUBC0fBGQ",
+        title:
+          "Hewan yang Bertahan Hidup dengan Memakan Racun - Literasi Sains",
+      },
+      {
+        youtubeId: "3Oj25ROIMKs",
+        title: "Kenapa Orang Gampang Percaya Hoax - Literasi Digital",
+      },
+    ],
+  },
+  {
     title: "Main Toca Boca",
     episodes: [
       {
@@ -65,6 +95,70 @@ export const dummyCerita = [
       {
         youtubeId: "p29KD122xZM ",
         title: "Drama Tersesat Ke Rumah Dracula - Toca Boca Eps 07 GoDuplo TV",
+      },
+    ],
+  },
+  {
+    title: "Cerita Rakyat Jawa dan DKI Jakarta",
+    episodes: [
+      {
+        youtubeId: "2tOyG9sxTWc",
+        title:
+          "Kisah Timun Mas dari Jawa Timur - Animasi Cerita Indonesia (ACI)",
+        isNew: true,
+      },
+      {
+        youtubeId: "s1VyUOxoqM",
+        title:
+          "Kisah Cindelaras dari Jawa Timur - Animasi Cerita Indonesia (ACI)",
+        isNew: true,
+      },
+      {
+        youtubeId: "i4A9I3V9vtA",
+        title:
+          "Kisah Si Pitung dari DKI Jakarta - Animasi Cerita Indonesia (ACI)",
+        isNew: true,
+      },
+      {
+        youtubeId: "5J8uGALWSTU",
+        title:
+          "Kisah Murtado, Macan Kemayoran dari DKI Jakarta - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "wzUIq9b_5Tc",
+        title:
+          "Kisah Asal-Usul Cianjur dari Jawa Barat - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "hSE-GFB-ENY",
+        title:
+          "Kisah Bawang Putih Bawang Merah dari Jawa Tengah - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "4K1t8xhW4kU",
+        title:
+          "Kisah Keong Mas dari Jawa Timur - Animasi Cerita Indonesia (ACI)",
+      },
+    ],
+  },
+  {
+    title: "BeaBeo Story",
+    episodes: [
+      {
+        youtubeId: "4bPylWTEPzs",
+        title: "Serunya Bermain Mobil Balap",
+      },
+      {
+        youtubeId: "9FEfWel3_mo",
+        title: "Hadiah Ulang Tahun Raya",
+      },
+      {
+        youtubeId: "DJEP6Bbo0S0",
+        title: "Bermain Layang-Layang",
+      },
+      {
+        youtubeId: "me2krIcCuoA",
+        title: "Hati Hati Orang Asing",
       },
     ],
   },
--- assets/media/musik.js
+++ assets/media/musik.js
@@ -44,4 +44,53 @@ export const dummyMusik = [
       },
     ],
   },
+  {
+    title: "Lagu Daerah / Nasional",
+    playlist: [
+      {
+        youtubeId: "UQB45z088yg",
+        title: "Lagu Hari Merdeka - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "VdZ17zFyMKE",
+        title:
+          "Lagu Daerah Anak Cublak-Cublak Suweng - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "sRyAhYEbMg0",
+        title:
+          "Lagu Daerah Anak Bungong Jeumpa - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "hGD3Gc02xjo",
+        title:
+          "Lagu Nasional Halo-Halo Bandung - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "pgfsl_0NEo4",
+        title: "Lagu Kicir-Kicir - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "XccYaaIq4qk",
+        title: "Lagu Cingcangkeling - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "6CkRTv0OUyo",
+        title:
+          "Lagu Kampuang Nan Jauh Di Mato - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "_cR8grqP3c8",
+        title: "Lagu Suwe Ora Jamu - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "HY_gTFciG70",
+        title: "Lagu Yamko Rambe Yamko - Animasi Cerita Indonesia (ACI)",
+      },
+      {
+        youtubeId: "4olSUubW9kA",
+        title: "Lagu Sinanggar Tulo - Animasi Cerita Indonesia (ACI)",
+      },
+    ],
+  },
 ];
